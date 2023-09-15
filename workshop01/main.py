f38f105291bc7f0ab1053191e02b3b3aa1f8b61b
Jerome Heng <jeromehenghaoxiang@gmail.com>
2023-09-15 13:53:02 +0800

    create simple web app

---
+++ workshop01/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.context_processor
+def get_quote():
+    quote_list = ["Logic will get you from A to B. Imagination will take you everywhere.",
+    "There are 10 kinds of people. Those who know binary and those who don't.",
+    "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+    "It's not that I'm so smart, it's just that I stay with problems longer.",
+    "It is pitch dark. You are likely to be eaten by a grue."]
+    return dict(quote=random.choice(quote_list))
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
