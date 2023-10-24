8a9efeab171055dc0e6a7b86b7e5f966f1145a27
kraftedcheese <jeromehenghaoxiang@gmail.com>
2023-10-24 16:30:51 +0800

    Finish day 01

--- workshop01/task01/dov-bear/python/main.py
+++ workshop01/task01/dov-bear/python/main.py
@@ -9,23 +9,29 @@ IMG_COUNT = 13
 
 cli_opt = parse_app_opts()
 
-app = Flask(__name__, static_folder=get_app_root('public'), static_url_path="/static")
+app = Flask(__name__, static_folder=get_app_root("public"), static_url_path="/static")
 
-@app.route('/')
+
+@app.route("/")
 def slash():
-   num = int(request.args['num'] if 'num' in request.args else '4')
-   img_list = list(range(0, IMG_COUNT))
-   random.shuffle(img_list)
-   idx = random.randint(0, IMG_COUNT - num)
+    num = int(request.args["num"] if "num" in request.args else "4")
+    img_list = list(range(0, IMG_COUNT))
+    random.shuffle(img_list)
+    idx = random.randint(0, IMG_COUNT - num)
 
-   return render_template('index.html', instance_name=cli_opt['name']
-         , instance_hash=cli_opt['hash'], dovs=img_list[idx: idx + num])
+    return render_template(
+        "index.html",
+        instance_name=cli_opt["name"],
+        instance_hash=cli_opt["hash"],
+        dovs=img_list[idx : idx + num],
+    )
 
-@app.route('/healthz')
+
+@app.route("/healthz")
 def healthz():
-   return {}, 204
+    return {}, 204
 
-if '__main__' == __name__:
-   print(f'Application started on port {cli_opt["port"]} at {datetime.now()}')
-   app.run(host='0.0.0.0', port=cli_opt['port'])
 
+if "__main__" == __name__:
+    print(f'Application started on port {cli_opt["port"]} at {datetime.now()}')
+    app.run(host="0.0.0.0", port=cli_opt["port"])
